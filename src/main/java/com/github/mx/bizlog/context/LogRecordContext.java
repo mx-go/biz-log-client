268134139bc7162961bed47fc193144a0d8d0180
max <1223585957@qq.com>
2021-05-27 22:24:29 +0800

    fix: 修复NPE

--- src/main/java/com/github/mx/bizlog/context/LogRecordContext.java
+++ src/main/java/com/github/mx/bizlog/context/LogRecordContext.java
@@ -1,23 +1,36 @@
 package com.github.mx.bizlog.context;
 
+import com.google.common.collect.Maps;
+
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 /**
+ * 自定义变量上下文
+ * <p>
  * Create by max on 2021/02/27
  **/
 public class LogRecordContext {
 
-    private static final InheritableThreadLocal<Map<String, Object>> variableMap = new InheritableThreadLocal<>();
+    private static final InheritableThreadLocal<Map<String, Object>> VARIABLES = new InheritableThreadLocal<>();
 
     public static void putVariable(String name, Object value) {
-        variableMap.get().put(name, value);
+        if (VARIABLES.get() == null) {
+            HashMap<String, Object> map = Maps.newHashMap();
+            map.put(name, value);
+            VARIABLES.set(map);
+        }
+        VARIABLES.get().put(name, value);
     }
 
     public static Map<String, Object> getVariables() {
-        return variableMap.get();
+        return Optional.ofNullable(VARIABLES.get()).orElse(Maps.newHashMap());
     }
 
     public static void clear() {
-        variableMap.get().clear();
+        if (VARIABLES.get() != null) {
+            VARIABLES.get().clear();
+        }
     }
 }
