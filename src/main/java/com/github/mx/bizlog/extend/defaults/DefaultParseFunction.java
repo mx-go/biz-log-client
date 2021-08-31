8f9afa381a126dfa5bb7b50bf94af8b0895e05ca
max <1223585957@qq.com>
2021-08-31 22:42:42 +0800

    build: 修改获取客户端浏览器和OS的jar

--- src/main/java/com/github/mx/bizlog/extend/defaults/DefaultParseFunction.java
+++ src/main/java/com/github/mx/bizlog/extend/defaults/DefaultParseFunction.java
@@ -1,10 +1,11 @@
 package com.github.mx.bizlog.extend.defaults;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.nacos.common.utils.StringUtils;
 import com.github.mx.bizlog.extend.ParseFunction;
 import com.github.mx.bizlog.util.LogHelper;
 import com.google.common.collect.Maps;
-import org.apache.commons.lang3.StringUtils;
+import eu.bitwalker.useragentutils.OperatingSystem;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
@@ -31,17 +32,21 @@ public class DefaultParseFunction implements ParseFunction {
             String ipAddr = LogHelper.getIpAddr(request);
             String browser = LogHelper.getBrowser(request);
             String os = LogHelper.getOs(request);
-            if (StringUtils.isNotBlank(ipAddr)) {
+            if (isLegal(ipAddr)) {
                 map.put("ip", LogHelper.getIpAddr(request));
             }
-            if (StringUtils.isNotBlank(browser)) {
+            if (isLegal(browser)) {
                 map.put("browser", browser);
             }
-            if (StringUtils.isNotBlank(os)) {
+            if (isLegal(os)) {
                 map.put("os", LogHelper.getOs(request));
             }
             return map.isEmpty() ? null : JSON.toJSONString(map);
         }
         return null;
+    }
+
+    private boolean isLegal(String param) {
+        return StringUtils.isNotBlank(param) && !OperatingSystem.UNKNOWN.getName().equalsIgnoreCase(param);
     }
 }
